716428245c7173231c4d1e4b93bfbc3607d26b32
Wiebe <wiebedg@gmail.com>
2025-06-13 16:36:56 +0200

    remove unnecessary information in modelingsettings

--- src/toolkit/defines/modelingsettings.py
+++ src/toolkit/defines/modelingsettings.py
@@ -7,6 +7,4 @@ class ModelingSettings():
     A data class to hold all settings for a given simulation run.
     """
     instrument: InstrumentBase
-    # distance_to_source: float
-    # source_radius: float
     exposure_time: float | None = None
