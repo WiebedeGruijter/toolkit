dc6c28b1416914f93d4f5342eaf73363105b144f
Wiebe <wiebedg@gmail.com>
2025-06-13 18:37:42 +0200

    restructure pytest

--- tests/datatypes/test_source.py
+++
@@ -1,7 +0,0 @@
-from toolkit.defines.modelingsettings import ModelingSettings
-from toolkit.defines.instrument import JWSTMiri
-
-def test_point_source():
-
-    path = r'C:\Users\wiebe\Repos\InterferometrySimulator\src\toolkit\test_data\blackbody_T2000.txt'
-
